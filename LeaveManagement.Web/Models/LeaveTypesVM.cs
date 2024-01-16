e0b98f55a5574e89520ea2cd65719487ad3010b2
Swati217 <swati.wankhede@publicissapient.com>
2024-01-16 11:31:28 +0530

    Modified the crud screens and added datatables and swal

---
+++ LeaveManagement.Web/Models/LeaveTypesVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypesVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        public int DefaultDays { get; set; }
+
+    }
+}
